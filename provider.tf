aa617a2e619068629eb07b276a172c502331ad36
Igor Brodewicz <igor@brodewicz.com>
2021-04-29 16:43:06 +0200

    Declare Route53 zone, define AZs and SG, spin out MSK cluster

--- provider.tf
+++ provider.tf
@@ -2,8 +2,12 @@ provider "aws" {
     region = "eu-central-1"
 }
 
+resource "aws_route53_zone" "primary" {
+  name = "brodewicz.tech"
+}
+
 resource "aws_vpc" "main" {
-    cidr_block = "10.0.0.0/24"
+    cidr_block = "10.0.0.0/16"
     instance_tenancy = "default"
 
     tags = {
@@ -13,7 +17,8 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "subnet1" {
     vpc_id = aws_vpc.main.id
-    cidr_block = "10.0.0.0/26"
+    availability_zone = "eu-central-1a"
+    cidr_block = "10.0.0.0/24"
 
     tags = {
         Name = "subnet1"
@@ -22,9 +27,50 @@ resource "aws_subnet" "subnet1" {
 
 resource "aws_subnet" "subnet2" {
     vpc_id = aws_vpc.main.id
-    cidr_block = "10.0.0.128/26"
+    availability_zone = "eu-central-1b"
+    cidr_block = "10.0.1.0/24"
 
     tags = {
         Name = "subnet2"
     }
+}
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "HTTP from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_http"
+  }
+}
+
+module "kafka" {
+  source                 = "github.com/cloudposse/terraform-aws-msk-apache-kafka-cluster.git?ref=master"
+  namespace              = "brodewicz.tech"
+  stage                  = "dev"
+  name                   = "streaming-app"
+  vpc_id                 = aws_vpc.main.id
+  zone_id                = aws_route53_zone.primary.id
+  security_groups        = [aws_security_group.allow_http.id]
+  subnet_ids             = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+  kafka_version          = "2.4.1.1"
+  number_of_broker_nodes = 2
+  broker_instance_type   = "kafka.m5.large"
 }
